3e4833ca226cfc121c5920b5285ec4c526819295
Rahullhub <youremail@example.com>
2025-06-18 20:10:51 +0530

    added virtuals conecept

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -20,5 +20,12 @@ tweetSchema.virtual('contentWithEmail').get(function process(){
    return this.content + "\n" + " Created by :" + this.userEmail;
 })
 
+//like in sequelize we have triggers , in mongoose we have middlewares
+tweetSchema.pre('save',function(next){
+   console.log('Inside a hook');
+   next();//calling a next miidleware
+})
+
+
 const Tweet = mongoose.model("Tweet", tweetSchema);
 module.exports=Tweet;
